aca963bdb6bd65dc863c8ef6c693c822ca2c62ab
Dolores Jadur <121775823+dolojadur@users.noreply.github.com>
2024-06-28 01:08:45 -0300

    limpio!!

--- src/main/java/com/example/demo/dtos/PrediccionDemandaDTO.java
+++ src/main/java/com/example/demo/dtos/PrediccionDemandaDTO.java
@@ -1,6 +1,7 @@
 package com.example.demo.dtos;
 
 import com.example.demo.entidades.Articulo;
+import com.example.demo.enums.CantidadPeriodo;
 import com.example.demo.enums.MetodoPrediccion;
 import jakarta.persistence.Enumerated;
 import lombok.Data;
@@ -10,13 +11,14 @@ import java.util.List;
 @Data
 public class PrediccionDemandaDTO {
 
-    private int cantidadDePredicciones;
+    private CantidadPeriodo cantidadDePredicciones;
     private Long idArticulo;
     private int mesAPredecir;
     private int anioAPredecir;
 
     //datos para crear prediccion que NO SE PIDEN EN EL FRONT
     private double error;
+    private double porcentajeDeError;
     private double prediccion;
 
     @Enumerated
